caf41ca20015516097a2922e6d3bc402cc856057
Rob Ashton <r.ashton@imperial.ac.uk>
2024-02-23 14:45:18 +0000

    Add end to end test

--- tests/testthat/test-zzz-e2e.R
+++ tests/testthat/test-zzz-e2e.R
@@ -1,8 +1,11 @@
+skip_if_not_installed("httr")
+root <- test_prepare_orderly_example(c("data", "parameters"))
+repo <- helper_add_git(root)
+bg <- porcelain::porcelain_background$new(api, list(root))
+bg$start()
+on.exit(bg$stop())
+
 test_that("can run server", {
-  skip_if_not_installed("httr")
-  root <- create_temporary_root(use_file_store = TRUE)
-  bg <- porcelain::porcelain_background$new(api, list(root))
-  bg$start()
   r <- bg$request("GET", "/")
   expect_equal(httr::status_code(r), 200)
 
@@ -13,4 +16,15 @@ test_that("can run server", {
                package_version_string("orderly2"))
   expect_equal(dat$data$orderly.runner,
                package_version_string("orderly.runner"))
+})
+
+test_that("can list reports", {
+  r <- bg$request("GET", "/report/list?hash=HEAD")
+  expect_equal(httr::status_code(r), 200)
+  
+  dat <- httr::content(r)
+  expect_equal(dat$status, "success")
+  expect_null(dat$errors)
+  reports <- vcapply(dat$data, "[[", "name")
+  expect_true(all(c("data", "parameters") %in% reports))
 })
