e011fa684f6d50b59a0b449990ad0364c07ebfe0
Mantra <mantrakusumgar@outlook.com>
2024-02-27 17:26:02 +0000

    refactor object so only has view on queue and does not manage workers

--- R/queue.R
+++ R/queue.R
@@ -8,6 +8,8 @@ Queue <- R6::R6Class("Queue", #nolint
     root = NULL,
     #' @field config Orderly config
     config = NULL,
+    #' @field controller RRQ controller
+    controller = NULL,
 
     #' @description
     #' Create object, read configuration and setup Redis connection.
@@ -15,12 +17,7 @@ Queue <- R6::R6Class("Queue", #nolint
     #' @param root Orderly root.
     #' @param queue_id ID of an existing queue to connect to, creates a new one
     #'   if NULL (default NULL)
-    #' @param workers Number of workers to spawn (default 1)
-    #' @param cleanup_on_exit If TRUE workers are killed on exit (defaults
-    #'   to TRUE if workers is > 0)
-    initialize = function(root, queue_id = NULL, workers = 1,
-                          cleanup_on_exit = workers > 0) {
-      private$cleanup_on_exit <- cleanup_on_exit
+    initialize = function(root, queue_id = NULL) {
       self$root <- root
       self$config <- orderly2::orderly_config(self$root)
       if (!runner_has_git(self$root)) {
@@ -28,42 +25,23 @@ Queue <- R6::R6Class("Queue", #nolint
                              "root is not version controlled."))
       }
 
-      # Connecting to Redis
-      private$con <- redux::hiredis()
-
       # Create queue
-      private$queue_id <- queue_id %||% orderly_queue_id()
-      private$queue <- rrq::rrq_controller$new(private$queue_id,
-                                               private$con)
+      self$controller <- rrq::rrq_controller2(
+        queue_id %||% orderly_queue_id()
+      )
       worker_config <- rrq::rrq_worker_config(heartbeat_period = 10)
-      private$queue$worker_config_save("localhost", worker_config)
-      private$start_workers(workers)
+      rrq::rrq_worker_config_save2("localhost", worker_config,
+                                   controller = self$controller)
     },
 
     # Just until we add queue status for testing
     number_of_workers = function() {
-      private$queue$worker_len()
+      rrq::rrq_worker_len(self$controller)
     },
 
-    #' @description stop workers and destroy queue if
-    #' cleanup_on_exit is TRUE and Redis connection available
+    #' @description Destroy queue
     finalize = function() {
-      if (private$cleanup_on_exit && !is.null(private$con)) {
-        private$queue$worker_stop(type = "kill")
-        private$queue$destroy(delete = TRUE)
-      }
-    }
-  ),
-  private = list(
-    cleanup_on_exit = NULL,
-    con = NULL,
-    queue = NULL,
-    queue_id = NULL,
-    start_workers = function(workers) {
-      if (workers > 0L) {
-        rrq::rrq_worker_spawn(private$queue, workers)
-      }
-      invisible()
+      rrq::rrq_destroy(self$controller)
     }
   ),
 )
@@ -73,6 +51,6 @@ runner_has_git <- function(path) {
 }
 
 orderly_queue_id <- function() {
-  id <- Sys.getenv("ORDERLY_SERVER_QUEUE_ID", "")
+  id <- Sys.getenv("ORDERLY_RUNNER_QUEUE_ID", "")
   if (nzchar(id)) id else sprintf("orderly.runner:%s", ids::random_id())
 }
