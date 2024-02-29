d2360eacfeccd69cff51997cf6ff7645fce3ad40
Rob Ashton <r.ashton@imperial.ac.uk>
2024-02-29 16:05:13 +0000

    Fix missing test coverage

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -5,8 +5,16 @@ test_that("null-or-value works", {
   expect_equal(NULL %||% 2, 2)
 })
 
+
 test_that("first_dirname gets the first dir part of the filename", {
   expect_equal(
     first_dirname(c("test/file/name.txt", "test", ".", "testing/file.txt")),
     c("test", "test", ".", "testing"))
+})
+
+
+test_that("sys_which", {
+  prog <- "a-path-that-does-not-exist"
+  expect_error(sys_which(prog),
+               "Did not find 'a-path-that-does-not-exist'")
 })
