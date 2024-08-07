9012df6f16e7d5840ba70b0ece1dae92b386886f
M-Kusumgar <mantrakusumgar@outlook.com>
2024-08-07 15:54:50 +0100

    add entrypoints + build and push gh actions workflow

--- R/main.R
+++ R/main.R
@@ -1,20 +1,41 @@
 parse_main <- function(args = commandArgs(TRUE)) {
   usage <- "Usage:
-orderly.runner [options] <path>
+orderly.runner.server [options] <path>
 
 Options:
   --log-level=LEVEL  Log-level (off, info, all) [default: info]
   --validate         Enable json schema validation
+  --host=HOST        Host to run api on [default: 0.0.0.0]
   --port=PORT        Port to run api on [default: 8001]"
   dat <- docopt::docopt(usage, args)
   list(log_level = dat$log_level,
        validate = dat$validate,
        port = as.integer(dat$port),
-       path = dat$path)
+       path = dat$path,
+       host = dat$host)
 }
-
 
 main <- function(args = commandArgs(TRUE)) {
   dat <- parse_main(args)
-  api(dat$path, dat$validate, dat$log_level)$run("0.0.0.0", port = dat$port)
+  api_obj <- api(dat$path, dat$validate, dat$log_level)
+  api_obj$run(host = dat$host, port = dat$port)
+}
+
+
+parse_main_worker <- function(args = commandArgs(TRUE)) {
+  usage <- "Usage:
+orderly.runner.worker [options] <path>
+
+Options:
+  --n-workers=N_WORKERS    Number of workers to spawn [default: 1]"
+  dat <- docopt::docopt(usage, args)
+  list(path = dat$path,
+       n_workers = as.integer(dat$n_workers))
+}
+
+main_worker <- function(args = commandArgs(TRUE)) {
+  dat <- parse_main_worker(args)
+  # assumes ORDERLY_RUNNER_QUEUE_ID is set
+  queue <- Queue$new(dat$path)
+  rrq::rrq_worker_spawn(dat$n_workers, controller = queue$controller)
 }
--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,24 +1,36 @@
-test_that("Can parse arguments", {
+test_that("Can parse arguments (server)", {
   expect_mapequal(parse_main("path"),
                   list(log_level = "info",
                        validate = FALSE,
                        port = 8001,
+                       host = "0.0.0.0",
                        path = "path"))
   expect_mapequal(parse_main(c("--port=8080", "path")),
                   list(log_level = "info",
                        validate = FALSE,
                        port = 8080,
+                       host = "0.0.0.0",
                        path = "path"))
   expect_mapequal(parse_main(c("--port=8080", "--validate", "path")),
                   list(log_level = "info",
                        validate = TRUE,
                        port = 8080,
+                       host = "0.0.0.0",
                        path = "path"))
   expect_mapequal(parse_main(c("--log-level=debug", "--validate", "path")),
                   list(log_level = "debug",
                        validate = TRUE,
                        port = 8001,
+                       host = "0.0.0.0",
                        path = "path"))
+  expect_mapequal(
+    parse_main(c("--host=host", "--log-level=debug", "--validate", "path")),
+    list(log_level = "debug",
+         validate = TRUE,
+         port = 8001,
+         host = "host",
+         path = "path")
+  )
 })
 
 
@@ -27,12 +39,42 @@ test_that("Can construct api", {
   mock_run <- mockery::mock()
   mock_api <- mockery::mock(list(run = mock_run))
   mockery::stub(main, "api", mock_api)
-  main(c("--log-level=debug", "path"))
+  main(c("--host=my-host", "--log-level=debug", "path"))
 
   mockery::expect_called(mock_api, 1)
   expect_equal(mockery::mock_args(mock_api)[[1]],
                list("path", FALSE, "debug"))
 
   mockery::expect_called(mock_run, 1)
-  expect_equal(mockery::mock_args(mock_run)[[1]], list("0.0.0.0", port = 8001))
+  expect_equal(mockery::mock_args(mock_run)[[1]],
+               list(host = "my-host", port = 8001))
+})
+
+test_that("Can parse arguments (worker)", {
+  expect_mapequal(parse_main_worker("path"),
+                  list(n_workers = 1,
+                       path = "path"))
+  expect_mapequal(parse_main_worker(c("--n-workers=2", "path")),
+                  list(n_workers = 2,
+                       path = "path"))
+})
+
+test_that("Can spawn workers", {
+  skip_if_not_installed("mockery")
+
+  mock_queue_new <- mockery::mock(list(controller = "test_controller"))
+  mockery::stub(main_worker, "Queue$new", mock_queue_new)
+
+  mock_rrq_worker_spawn <- mockery::mock()
+  mockery::stub(main_worker, "rrq::rrq_worker_spawn", mock_rrq_worker_spawn)
+
+  main_worker(c("--n-workers=8128", "path"))
+
+  mockery::expect_called(mock_queue_new, 1)
+  expect_equal(mockery::mock_args(mock_queue_new)[[1]],
+               list("path"))
+
+  mockery::expect_called(mock_rrq_worker_spawn, 1)
+  expect_equal(mockery::mock_args(mock_rrq_worker_spawn)[[1]],
+               list(8128, controller = "test_controller"))
 })
